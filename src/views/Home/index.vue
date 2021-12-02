9a42408fd1412ad90f32d9d3a03cc208cb466f7f
zuishuainanhai <1705902650@qq.com>
2021-12-02 22:08:43 +0800

    less自动化导入

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -12,7 +12,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-@import "~@/styles/variables.less";
+// @import "~@/styles/variables.less";
 .box {
   color: @xtxColor;
 }
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,23 @@
+const path = require('path')
 module.exports = {
   lintOnSave: true,
+
   // 打包后可以部署到任意目录
   // publicPath: './',
   devServer: {
     open: true
+  },
+  // 关于vue add style-resources-loader样式插件配置(webpack、、js/json用loader)
+  pluginOptions: {
+    // 配置加载器
+    'style-resources-loader': {
+      // 预处理器风格
+      preProcessor: 'less',
+      // 配置那些文件自动注入到页面中
+      patterns: [
+        // 可以为多个
+        path.join(__dirname, './src/styles/variables.less')
+      ]
+    }
   }
 }
