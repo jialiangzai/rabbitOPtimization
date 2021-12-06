752af0f0bdc0902d4c483eb3f44a8b9e038240a9
zuishuainanhai <1705902650@qq.com>
2021-12-06 19:46:13 +0800

    新增蒙层滑块和大图结构

--- src/views/Goods/components/goods-image.vue
+++ src/views/Goods/components/goods-image.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="goods-image">
+    <!-- 放大镜效果盒子 -->
+    <div
+      class="large"
+      :style="[{ backgroundImage: `url(${imgs[currentIndex]})` }]"
+    ></div>
     <div class="middle">
       <!-- 目前写死了显示第一个 -->
       <img :src="imgs[currentIndex]" alt="" />
+      <!-- 蒙层容器 -->
+      <div class="layer"></div>
     </div>
+    <!-- 图片列表 -->
     <ul class="small">
       <li v-for="(img, i) in imgs" :key="i" @mouseenter="currentIndex = i">
         <img :src="img" alt="" />
@@ -23,6 +31,12 @@ export default {
   }, /**
    * 实现鼠标进入右侧列表左侧显示不同大图片
    * 因为imgs是一个数组元素是地址，只要根据下标更改不同的元素显示不同图片
+   */
+  /**
+   * 放大镜 难点
+   * 1. 准备蒙层
+   * 2. 鼠标在左侧预览盒子内移动蒙层以中心点-----鼠标的移动距离和坐标
+   * 3. 放大镜区域随着蒙层移动实现移动----backgroundposition
    */
   setup () {
     const currentIndex = ref(0)
@@ -57,5 +71,27 @@ export default {
       }
     }
   }
+}
+// 放大镜
+.large {
+  position: absolute;
+  top: 0;
+  left: 412px;
+  width: 400px;
+  height: 400px;
+  z-index: 500;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-repeat: no-repeat;
+  background-size: 800px 800px;
+  background-color: #f8f8f8;
+}
+// 蒙层
+.layer {
+  width: 200px;
+  height: 200px;
+  background: rgba(0, 0, 0, 0.2);
+  left: 0;
+  top: 0;
+  position: absolute;
 }
 </style>
